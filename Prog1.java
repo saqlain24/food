c6de6babf124454346fe18bb15630f7bdb7cdf3c
saqlain24 <53937464+saqlain24@users.noreply.github.com>
2019-09-24 16:48:37 +0530

    Update Prog1.java

--- Prog1.java
+++ Prog1.java
@@ -7,6 +7,8 @@ public static void main(String[] args)
 
         System.out.println(args[0] + " Technologies " + args[1]);
 	System.out.println(args[0] + " Technologies " + args[2]);
+	System.out.println(args[0] + " Technologies " + args[3]);
+	System.out.println(args[0] + " Technologies " + args[4]);
 
     }
 
