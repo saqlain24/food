960b71320e0849d5cfc63790fe351a66c573091f
saqlain24 <53937464+saqlain24@users.noreply.github.com>
2019-09-26 21:22:07 +0530

    mew vvv

--- Prog1.java
+++ Prog1.java
@@ -4,7 +4,8 @@ class Prog1 {
     
 public static void main(String[] args) 
 {
-
+huutyutygyhutgfyhtgfyhtgfyhtgfyhtfyhtfgyhtgfhtgfhctgf
+	jhjuygh
         System.out.println(args[0] + " Technologies " + args[1]);
 	System.out.println(args[0] + " Technologies " + args[2]);
 	System.out.println(args[0] + " Technologies " + args[3]);
